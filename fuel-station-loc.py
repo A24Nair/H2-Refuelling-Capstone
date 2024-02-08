41fc4e2bbb9697ac6373b2596c6cf62addb3c273
VKHLeung <vincent.kahang.leung@gmail.com>
2024-02-07 23:06:56 -0500

    added general logic into truck route generation

--- fuel-station-loc.py
+++ fuel-station-loc.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import requests
 import json
+import random
 
 # AN Bing Maps API key
 bing_maps_api_key = "AkG58SoxLujRYGeH30ePGMT_9gz3aQ6UCiGBn3EeZ5BO6d-2OYlos9L1yH0gbopC"
@@ -43,11 +44,34 @@ def find_travel_time_distance(start_lat,start_long,end_lat,end_long):
 	travel_time = data['resourceSets'][0]['resources'][0]['results'][0]['travelDuration']
 	return [travel_distance,travel_time]
 
+def system_wide_sim(tm):
+    truck_num = np.arange(0,150) #generates 150 numbers
+    truck_num = np.array(truck_num).reshape(150,1)
+    tube_num = np.array([401,402,403,404,405,406])
+    tube_num = np.array(tube_num).reshape(6,1)
+    start_dest = end_dest = fuel = rnge = np.ones((156,1))
+    dest_node = np.array([tm.iloc[12:,0]]).flatten().tolist()
+    probabilities = np.array([0.1,0.1,0.1,0.05,0.05,0.1,0.3,0.2])
+    
+    for i in truck_num:
+        start_dest[i,0] *= random.randint(13,tm.iloc[-1,0])
+        end_node = np.random.choice(dest_node, p = probabilities)
+        while end_node == start_dest[i,0]:
+            end_node = np.random.choice(dest_node, p = probabilities)
+        end_dest[i,0] *= end_node
+        fuel[i,0] = random.uniform(30,60)
+        rnge[i,0] = 700 * fuel[i,0]/60
+    start_dest[150,0] = start_dest[151,0] = start_dest[152,0] = 3
+    start_dest[153,0] = start_dest[154,0] = start_dest[155,0] = 8
+    
+    return None
+
 if __name__ == "__main__":
     excel_file_path = "Travel Matrix.xlsx" # Enter filepath here
-    df = pd.read_excel(excel_file_path,sheet_name="Sheet2 (2)",usecols="A:G",nrows=19) # Missing: build logic for last row with non empty postal code
+    df = pd.read_excel(excel_file_path,sheet_name="Sheet2 (2)",usecols="A:G",nrows=22) # Missing: build logic for last row with non empty postal code
     # Apply the function
     df[["Latitude", "Longitude"]] = df.apply(find_location, axis=1)
     #print(df)
-    tester = find_travel_time_distance(df.iloc[5,7],df.iloc[5,8],df.iloc[18,7],df.iloc[18,8])
-    print(tester)
+    tester = find_travel_time_distance(df.iloc[5,7],df.iloc[5,8],df.iloc[18,7],df.iloc[18,8]) #this function can be used to extract time and distance -> to work on it more
+    travel_matrix = pd.read_excel(excel_file_path,sheet_name="Sheet2",usecols="A:U",nrows=22)
+    sys = system_wide_sim(travel_matrix)
