4fe50c6d1955600da5d6abe732314e6c6d3243f3
A24Nair <anand24nair@gmail.com>
2024-03-17 18:44:18 -0400

    final results for 102 simulations

--- results-summary.py
+++ results-summary.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import os
-from datetime import datetime
 
 def save_sim_files():
     current_dir = os.getcwd()
@@ -41,7 +40,7 @@ def summary_report(paths):
     avg_stops = sum(truck_count_refueling_stops)/len(truck_count_refueling_stops)
     avg_daily_hyd_consump = (avg_dist*50/750)*150 # 50 kg gives 750km, 150 vehicles
 
-    row_labels = ['Average distance travelled','Max. distance','Min. distance','Average travel time (h)','Max. travel time (h)','Min. travel time (h)','Average number of refuelling stops','Max. number of refuelling stops','Avg. daily hydrogen consumption (150 trucks, kg)','Number of simulations run']
+    row_labels = ['Average distance travelled (km)','Max. distance (km)','Min. distance (km)','Average travel time (h)','Max. travel time (h)','Min. travel time (h)','Average number of refuelling stops','Max. number of refuelling stops','Avg. daily hydrogen consumption (150 trucks, kg)','Number of simulations run']
     row_vals = [round(avg_dist), round(max_dist), round(min_dist), round(avg_time, 1), round(max_time, 1), round(min_time, 1), round(avg_stops), round(max(truck_count_refueling_stops)),round(avg_daily_hyd_consump,1),num_simulations]
     pd.DataFrame({'Parameter': row_labels, 'Value': row_vals}).to_excel('Truck-Travel-Summary.xlsx',index=False)
     
@@ -75,7 +74,7 @@ def summary_report(paths):
             file_path = os.path.join(paths[2], file)
 
             df = pd.read_excel(file_path,sheet_name=['Sheet1', 'Sheet2'])
-            for col_name in df.columns[1:]:
+            for col_name in df['Sheet1'].columns[1:]:
                 df_dict[col_name].at[file_num,'Trucks Visited'] = df['Sheet1'][col_name].sum()
                 df_dict[col_name].at[file_num,'Trucks Refuelled'] = df['Sheet2'][col_name].sum()
     
@@ -83,23 +82,28 @@ def summary_report(paths):
     max_cons = []
     avg_total_cons = []
     avg_truck_visit = []
+    total_truck_visit = []
     avg_truck_refuel = []
+    total_truck_refuel = []
     avg_reliability = []
     row_labels = [f'Station {i+1}' for i in range(num_fuel_stations)]
     for index,val in enumerate(row_labels):
-        max_cons.append(df_dict[val]['Maximum consumption'].max())
-        min_cons.append(df_dict[val]['Minimum consumption'].max())
-        avg_total_cons.append(df_dict[val]['Total consumption'].mean())
+        max_cons.append(round(df_dict[val]['Total consumption']).max())
+        min_cons.append(round(df_dict[val]['Total consumption']).min())
+        avg_total_cons.append(round(df_dict[val]['Total consumption'].mean()))
         station_visit_avg = df_dict[val]['Trucks Visited'].mean()
         station_refuel_avg = df_dict[val]['Trucks Refuelled'].mean()
         avg_truck_visit.append(round(station_visit_avg))
         avg_truck_refuel.append(round(station_refuel_avg))
+
+        total_truck_visit.append(df_dict[val]['Trucks Visited'].sum())
+        total_truck_refuel.append(df_dict[val]['Trucks Refuelled'].sum())
         if station_visit_avg == 0:
             avg_reliability.append(100)
         else:
-            avg_reliability.append(round(station_visit_avg/station_refuel_avg)*100)
+            avg_reliability.append(round(total_truck_refuel[index]/total_truck_visit[index],5)*100)
 
-    pd.DataFrame({'Station': row_labels, 'Maximum consumption': max_cons,'Minimum consumption':max_cons,'Total consumption':avg_total_cons,'Trucks Visited':avg_truck_visit,'Trucks Refuelled':avg_truck_refuel,'Station Reliability':avg_reliability}).to_excel('Fuel-Station-Summary.xlsx',index=False)
+    pd.DataFrame({'Station': row_labels, 'Maximum consumption': max_cons,'Minimum consumption':min_cons,'Total consumption':avg_total_cons,'Trucks Visited':avg_truck_visit,'Trucks Refuelled':avg_truck_refuel,'Total Trucks Visited':total_truck_visit,'Total Trucks Refuelled':total_truck_refuel,'Station Reliability':avg_reliability}).to_excel('Fuel-Station-Summary.xlsx',index=False)
 
     return
 
