6e9646c0d4bc94829516d93364a9572137575ea4
A24Nair <anand24nair@gmail.com>
2024-03-17 17:39:47 -0400

    Testing results summary - code structure is complete for fuel consumption and truck travel summary

--- results-summary.py
+++ results-summary.py
@@ -19,17 +19,15 @@ def save_sim_files():
     return (path1,path2,path3,path4,path5)
 
 def summary_report(paths):
-    # paths[0] = simulation data - time
-    # paths[1] = final conditions for each truck
 
     # -------------------------Summary 1 - trucks ----------------------
     truck_dist = []
     truck_time = []
     truck_count_refueling_stops = []
 
-    for file_num,file in enumerate(os.listdir(paths[1]),start=1): # Lists all files in the folder and iterates over all
+    for file_num,file in enumerate(os.listdir(paths[0]),start=1): # Lists all files in the folder and iterates over all
         if file.endswith('.xlsx'): # Check if file is excel file
-            file_path = os.path.join(paths[1], file)
+            file_path = os.path.join(paths[0], file)
             df = pd.read_excel(file_path)
             truck_time.append(df.loc[:149,"Travel Time"].values)
             truck_dist.append(df.loc[:149,"Travel Range"].values)
@@ -47,47 +45,64 @@ def summary_report(paths):
     row_vals = [round(avg_dist), round(max_dist), round(min_dist), round(avg_time, 1), round(max_time, 1), round(min_time, 1), round(avg_stops), round(max(truck_count_refueling_stops)),round(avg_daily_hyd_consump,1),num_simulations]
     pd.DataFrame({'Parameter': row_labels, 'Value': row_vals}).to_excel('Truck-Travel-Summary.xlsx',index=False)
     
-    # Sheet 2 - fuel station consumption
+    # ------------------------------------------Summary 2 fuel station consumption -----------------------------------------
     # Rows = fuel stations
     # Total
-    # Cols = average consumption(last row avg across all files), max consumption(across all files), min consumption(across all files)
+    # Cols = average consumption(last row avg across all files), max consumption(across all files), min consumption(across all files), reliability
     num_fuel_stations = 12
     df_dict={}
     for i in range(num_fuel_stations):
         data = {'Maximum consumption': np.zeros(num_simulations),
                 'Minimum consumption': np.zeros(num_simulations),
-                'Total Consumption': np.zeros(num_simulations),
+                'Total consumption': np.zeros(num_simulations),
                 'Trucks Visited': np.zeros(num_simulations),
                 'Trucks Refuelled': np.zeros(num_simulations)}
         
         stat_summary_df = pd.DataFrame(data)
         df_dict[f'Station {i+1}'] = stat_summary_df
 
-    for file_num,file in enumerate(os.listdir(paths[2])): # Lists all files in the folder and iterates over all
+    for file_num,file in enumerate(os.listdir(paths[1])): # Lists all files in the folder and iterates over all
         if file.endswith('.xlsx'): # Check if file is excel file
-            file_path = os.path.join(paths[2], file)
+            file_path = os.path.join(paths[1], file)
             df = pd.read_excel(file_path)
             for col_name in df.columns[1:]:
                 df_dict[col_name].at[file_num,'Maximum consumption'] = df[col_name].max()
                 df_dict[col_name].at[file_num,'Minimum consumption'] = df[col_name].min()
                 df_dict[col_name].at[file_num,'Total consumption'] = df[col_name].sum()
     
-    for file_num,file in enumerate(os.listdir(paths[3])): # Lists all files in the folder and iterates over all
+    for file_num,file in enumerate(os.listdir(paths[2])): # Lists all files in the folder and iterates over all
         if file.endswith('.xlsx'): # Check if file is excel file
-            file_path = os.path.join(paths[3], file)
+            file_path = os.path.join(paths[2], file)
 
             df = pd.read_excel(file_path,sheet_name=['Sheet1', 'Sheet2'])
             for col_name in df.columns[1:]:
                 df_dict[col_name].at[file_num,'Trucks Visited'] = df['Sheet1'][col_name].sum()
                 df_dict[col_name].at[file_num,'Trucks Refuelled'] = df['Sheet2'][col_name].sum()
-
+    
+    min_cons = []
+    max_cons = []
+    avg_total_cons = []
+    avg_truck_visit = []
+    avg_truck_refuel = []
+    avg_reliability = []
     row_labels = [f'Station {i+1}' for i in range(num_fuel_stations)]
-    row_vals = [round(avg_dist), round(max_dist), round(min_dist), round(avg_time, 1), round(max_time, 1), round(min_time, 1), round(avg_stops), round(max(truck_count_refueling_stops)),round(avg_daily_hyd_consump,1)]
+    for index,val in enumerate(row_labels):
+        max_cons.append(df_dict[val]['Maximum consumption'].max())
+        min_cons.append(df_dict[val]['Minimum consumption'].max())
+        avg_total_cons.append(df_dict[val]['Total consumption'].mean())
+        station_visit_avg = df_dict[val]['Trucks Visited'].mean()
+        station_refuel_avg = df_dict[val]['Trucks Refuelled'].mean()
+        avg_truck_visit.append(round(station_visit_avg))
+        avg_truck_refuel.append(round(station_refuel_avg))
+        if station_visit_avg == 0:
+            avg_reliability.append(100)
+        else:
+            avg_reliability.append(round(station_visit_avg/station_refuel_avg)*100)
 
-    pd.DataFrame({'Station': row_labels, 'Maximum consumption': row_vals,'Minimum consumption':,'Total Consumption':,'Trucks Visited':,'Trucks Refuelled':}).to_excel('Fuel-Station-Summary.xlsx',index=False)
+    pd.DataFrame({'Station': row_labels, 'Maximum consumption': max_cons,'Minimum consumption':max_cons,'Total consumption':avg_total_cons,'Trucks Visited':avg_truck_visit,'Trucks Refuelled':avg_truck_refuel,'Station Reliability':avg_reliability}).to_excel('Fuel-Station-Summary.xlsx',index=False)
 
     return
 
 if __name__ == "__main__":
     paths = save_sim_files() # Create folders to save files
-    summary_report(paths[1:])
+    summary_report(paths[2:])
